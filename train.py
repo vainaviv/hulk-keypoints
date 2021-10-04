5d2c0dc07b70be2ccac1fd20eb6b97dafc8bb8c4
Vainavi Viswanath <vainavi@mason.ist.berkeley.edu>
2021-10-03 23:11:26 -0700

    stuff for multimodal

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ def fit(train_data, test_data, model, epochs, checkpoint_path = ''):
 
 # dataset
 workers=0
-dataset_dir = 'hulkL_loop'
+dataset_dir = 'endpoints_more'
 output_dir = 'checkpoints'
 save_dir = os.path.join(output_dir, dataset_dir)
 
