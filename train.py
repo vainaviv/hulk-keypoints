c2b6376723bd482733cd7c24643ff6edcac32026
Vainavi Viswanath <vainavi@mason.ist.berkeley.edu>
2021-10-25 22:47:44 -0700

    stuff

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ def fit(train_data, test_data, model, epochs, checkpoint_path = ''):
 
 # dataset
 workers=0
-dataset_dir = 'hulkL_seg'
+dataset_dir = 'endpoints_new'
 output_dir = 'checkpoints'
 save_dir = os.path.join(output_dir, dataset_dir)
 
