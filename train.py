fbd89edae4ff397c1d219bf51b3c79007f74df65
Vainavi Viswanath <vainavi@mason.ist.berkeley.edu>
2021-10-26 09:32:33 -0700

    running stuff

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from src.dataset import KeypointsDataset, transform
 MSE = torch.nn.MSELoss()
 bceLoss = nn.BCELoss
 
-os.environ["CUDA_VISIBLE_DEVICES"]="4"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 def forward(sample_batched, model):
     img, gt_gauss = sample_batched
