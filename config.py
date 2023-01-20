2db871015741ebab662e8ddb49b578ee51123260
Kaushik Shivakumar <19kaushiks@gmail.com>
2023-01-20 18:26:55 +0000

    save latest preprocessing/changes

--- config.py
+++ config.py
@@ -1020,6 +1020,59 @@ class UNDER_OVER_RNet34_lr1e5_medley_03Hard2(BaseConfig):
     sharpen: bool = True
     learning_rate: float = 1.0e-5
 
+
+@dataclass
+class UNDER_OVER_RNet34_lr1e5_medley_03Hard2_wReal(BaseConfig):
+    expt_type: str = ExperimentTypes.CLASSIFY_OVER_UNDER
+    dataset_dir: str = field(default_factory=lambda: ['/home/vainavi/hulk-keypoints/processed_sim_data/under_over_centered_hard1', '/home/vainavi/hulk-keypoints/processed_sim_data/under_over_centered_hard2/', '/home/kaushiks/hulk-keypoints/processed_sim_data/under_over_REAL_centered/'])
+    dataset_weights: List[float] = field(default_factory=lambda: [0.7, 0.3, 0.2])
+    dataset_real: List[bool] = field(default_factory=lambda: [False, False, True])
+    classes: int = 1
+    img_height: int = 20
+    img_width: int = 20
+    crop_width: int = 10
+    num_keypoints: int = 1
+    gauss_sigma: int = 2
+    epochs: int = 50
+    batch_size: int = 4
+    cond_point_dist_px: int = 20
+    condition_len: int = 5
+    pred_len: int = 0
+    eval_checkpoint_freq: int = 1
+    min_checkpoint_freq: int = 10
+    resnet_type: str = '34'
+    pretrained: bool = True
+    rot_cond: bool = True
+    sharpen: bool = True
+    learning_rate: float = 1.0e-5
+
+
+@dataclass
+class UNDER_OVER_RNet34_lr1e5_RealOnly(BaseConfig):
+    expt_type: str = ExperimentTypes.CLASSIFY_OVER_UNDER
+    dataset_dir: str = field(default_factory=lambda: ['/home/kaushiks/hulk-keypoints/processed_sim_data/under_over_REAL_centered/'])
+    dataset_weights: List[float] = field(default_factory=lambda: [1.0])
+    dataset_real: List[bool] = field(default_factory=lambda: [True])
+    classes: int = 1
+    img_height: int = 20
+    img_width: int = 20
+    crop_width: int = 10
+    num_keypoints: int = 1
+    gauss_sigma: int = 2
+    epochs: int = 50
+    batch_size: int = 4
+    cond_point_dist_px: int = 20
+    condition_len: int = 5
+    pred_len: int = 0
+    eval_checkpoint_freq: int = 1
+    min_checkpoint_freq: int = 10
+    resnet_type: str = '34'
+    pretrained: bool = False
+    rot_cond: bool = True
+    sharpen: bool = True
+    learning_rate: float = 1.0e-5
+
+
 @dataclass
 class UNDER_OVER_RNet34_lr1e5_medley_04Hard2(BaseConfig):
     expt_type: str = ExperimentTypes.CLASSIFY_OVER_UNDER
@@ -1117,6 +1170,8 @@ UNDER_OVER_RNet34_lr1e5_hard1, #original
 UNDER_OVER_RNet34_lr1e6_hard1,
 UNDER_OVER_RNet34_lr1e7_hard1,
 UNDER_OVER_RNet34_lr1e5_medley_03Hard2,
+UNDER_OVER_RNet34_lr1e5_medley_03Hard2_wReal,
+UNDER_OVER_RNet34_lr1e5_RealOnly,
 UNDER_OVER_RNet34_lr1e5_medley_04Hard2,
 UNDER_OVER_NONE]
 
